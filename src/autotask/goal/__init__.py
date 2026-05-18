fe12adad54b2e1c4e9a5fdf123b21111d6f35032
Navigator <yuanyedmc@hotmail.com>
2026-05-18 10:36:28 +0800

    feat: 添加 src/autotask/goal/__init__.py

---
+++ src/autotask/goal/__init__.py
@@ -0,0 +1,13 @@
+"""
+Goal 模块 - 目标管理
+"""
+
+from autotask.goal.goal_manager import GoalManager, Goal
+from autotask.goal.milestone_tracker import MilestoneTracker, Milestone
+
+__all__ = [
+    "GoalManager",
+    "Goal",
+    "MilestoneTracker",
+    "Milestone",
+]
